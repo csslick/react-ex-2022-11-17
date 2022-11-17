bf5fe7d143cf065e0e6f589f0d4d40d2dec7e4b4
csslick <digitone@naver.com>
2022-11-17 17:58:13 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ColorButton from "./components/ColorButton";
+
+function App() {
+  
+  return (
+    <>
+      <h1 style={{ color: "red" }}>Hello</h1>
+      <ColorButton />
+    </>
+  );
+}
+
+export default App;
